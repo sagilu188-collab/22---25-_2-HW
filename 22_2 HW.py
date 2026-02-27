4d1733a94fe5c323c342ec06f24c42e19e31493f
sagilu188-collab <sagilu188@gmail.com>
2026-02-27 23:31:42 +0200

    Add files via upload

---
+++ 22_2 HW.py
@@ -0,0 +1,7 @@
+def print_numbers(start, stop):
+    for i in range(start, stop + 1):
+        print(i)
+num1 = int(input("Enter start number: "))
+num2 = int(input("Enter stop number: "))
+
+print_numbers(num1, num2)
